85c4bb8c89af2c412aa25c6a5513ee46aff244cc
mohammed warrag <m.warrag007@gmail.com>
2020-07-08 17:14:00 +0300

    update uom

--- terms_condition/static/src/js/terms_condition.js
+++ terms_condition/static/src/js/terms_condition.js
@@ -1,11 +1,10 @@
-odoo.define('terms_conditionadd_medicines.terms_conditionadd_medicines', function (require) {
+odoo.define('terms_condition.terms_conditionadd_medicines', function (require) {
 "use strict";
     var ajax = require('web.ajax');
 
     $(document).ready(function () {
 
         $('.checkbox_cgv').change(function() {
-
             ajax.jsonRpc('/check/terms-and-condition', 'call', {
                 'aggrement': $('.checkbox_cgv').prop('checked')
             });
