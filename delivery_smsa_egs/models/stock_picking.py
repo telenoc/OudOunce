b8d9f962d1070f8862bc4841437c4308f3838ed8
Abc Xyz <abc@abc.com>
2021-03-17 14:30:58 +0530

    [REM] Remove

--- delivery_smsa_egs/models/stock_picking.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import fields, models
-
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    cancel_reason = fields.Char()
